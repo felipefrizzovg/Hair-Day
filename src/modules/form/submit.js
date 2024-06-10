d47a79da77facd53e8464fa1c458ca29361616f0
Felipe Frizzo <felipe.garrido@buscacliente.com.br>
2024-06-10 18:05:41 -0300

    Sending the selected hour to the API

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,6 +1,8 @@
 import dayjs from 'dayjs'
+
 const form = document.querySelector("form")
 const selectedDate = document.getElementById("date")
+const clientName = document.getElementById("client")
 
 // Date atual para formatar o input
 const inputToday = dayjs(new Date()).format("YYYY-MM-DD")
@@ -13,4 +15,36 @@ form.onsubmit = (event) => {
   // Previne o comportamento padrão de carregar a pagina 
   event.preventDefault();
 
+  try {
+    // recuperando o nome do cliente
+    const name = clientName.value.trim()
+
+    if(!name) {
+      return alert("Informe o nome do cliente")
+    }
+
+    // Recupera o horario selecionado
+    const hourSelected = document.querySelector(".hour-selected")
+
+    // Verifica se o horario foi selecionado
+    if(!hourSelected) {
+      return alert("Selecione um horário")
+    }
+
+    // Recupera somente a hora
+    const [hour] = hourSelected.innerText.split(":")
+
+    // insera a hora na data
+    const when = dayjs(selectedDate.value).add(hour, "hour")
+
+    // Gera um ID
+    const id = new Date().getTime()
+
+    console.log({
+      id, name, when
+    })
+  } catch (error) {
+    alert("Não foi possível	realizar o agendamento")
+    console.log(error)
+  }
 }
